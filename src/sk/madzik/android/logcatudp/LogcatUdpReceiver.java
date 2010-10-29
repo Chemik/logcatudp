177e354e8578a24699d8d8dafd1cafbde0e7fadb
jmadzik <Chemik@users.noreply.github.com>
2010-10-29 17:08:01 +0200

    Implemented start at boot option.

---
+++ src/sk/madzik/android/logcatudp/LogcatUdpReceiver.java
@@ -0,0 +1,22 @@
+package sk.madzik.android.logcatudp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+public class LogcatUdpReceiver extends BroadcastReceiver {
+	private static final String TAG = "LogcaUdpReceiver";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d(TAG, "Boot completed; starting service ...");
+		SharedPreferences settings = context.getSharedPreferences(LogcatUdpCfg.Preferences.PREFS_NAME, Context.MODE_PRIVATE);
+
+		if (settings.getBoolean(LogcatUdpCfg.Preferences.AUTO_START, true)) {
+			context.startService(new Intent(context, LogcatUdpService.class));
+			Log.i(TAG, "Service Started");
+		}
+	}
+}
